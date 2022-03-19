128d011a4aec647d7aa44ef5ec20fbbcc6fdb926
Antônio Schappo <antonio.schappo@mercadolivre.com>
2022-03-19 16:52:21 -0300

    Finish ExerciseOne for March 18th class

--- src/main/java/com/example/demo/ExerciseOne.java
+++ src/main/java/com/example/demo/ExerciseOne.java
@@ -1,4 +1,26 @@
 package com.example.demo;
 
+import org.apache.tomcat.jni.Local;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+@RequestMapping
 public class ExerciseOne {
+
+    @GetMapping(path= "/{day}/{month}/{year}")
+    public Integer calcIdade(@PathVariable Integer day,
+                            @PathVariable Integer month,
+                            @PathVariable Integer year) {
+        LocalDate today = LocalDate.now();
+        LocalDate birth = LocalDate.of(year, month, day);
+        return Period.between(birth, today).getYears();
+    }
 }
+
+// error treatment to be implemented
